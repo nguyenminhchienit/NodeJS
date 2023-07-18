c258331e8217282801cb657577dd54da4aa2779e
NguyenMinhChien <minhchien03112003@gmail.com>
2023-07-18 22:29:53 +0700

    ROUTERS & MVC DESIGN PATTERN VỚI NODE.JS

---
+++ src/route/web.js
@@ -0,0 +1,16 @@
+import express from "express";
+import homeController from '../controller/homeController'
+
+let router = express.Router();
+
+const initWebRouter = (app) => {
+    router.get('/', homeController.getHomePage)
+      
+    router.get('/home', (req, res) => {
+          res.send('Welcome to Takis');
+    })
+
+    return app.use('/',router);
+}
+
+export default initWebRouter;
--- src/server.js
+++ src/server.js
@@ -2,19 +2,17 @@ import express from "express";
 import configViewEngine from "./config/configViewEngine";
 // require('dotenv').config;
 import {} from 'dotenv/config' 
+import initWebRouter from "./route/web";
 
 const app = express()
 const port = process.env.PORT || 8080
 console.log(port)
 
+//
 configViewEngine(app)
-app.get('/', (req, res) => {
-  res.render('index.ejs')
-})
 
-app.get('/home', (req, res) => {
-    res.send('Welcome to Takis');
-  })
+//
+initWebRouter(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
